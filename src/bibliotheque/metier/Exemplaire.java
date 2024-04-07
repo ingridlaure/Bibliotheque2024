efca5aa641686969175ef27607d9b3925f864c0c
ingrid <ingrid.ngounekenfack@condorce.be>
2024-04-07 21:23:14 +0200

    modification methode enLocation et lecteurActuel

--- src/bibliotheque/metier/Exemplaire.java
+++ src/bibliotheque/metier/Exemplaire.java
@@ -5,6 +5,9 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import bibliotheque.gestion.Gestion;
+
+import static bibliotheque.gestion.Gestion.lloc;
 
 public class Exemplaire {
 
@@ -16,8 +19,6 @@ public class Exemplaire {
 
     private String etat;
 
-
-    private List<Location> lloc= new ArrayList<>();
 
 
     public Exemplaire(String matricule, String descriptionEtat,Ouvrage ouvrage){
@@ -77,13 +78,9 @@ public class Exemplaire {
         this.rayon.getLex().add(this);
     }
 
-    public List<Location> getLloc() {
-        return lloc;
-    }
 
-    public void setLloc(List<Location> lloc) {
-        this.lloc = lloc;
-    }
+
+
 
     @Override
     public String toString() {
@@ -100,55 +97,20 @@ public class Exemplaire {
     }
 
     public Lecteur lecteurActuel(){
-        if(enLocation()) return lloc.get(lloc.size()-1).getLoueur();
+        if(enLocation()) return lloc.get(this);
         return null;
     }
-    public List<Lecteur> lecteurs(){
-        List<Lecteur> ll = new ArrayList<>();
-        for(Location l : lloc){
-            if(ll.contains(l)) continue; //par la suite utiliser set
-            ll.add(l.getLoueur());
-        }
-        return null;
-    }
+
 
     public void envoiMailLecteurActuel(Mail mail){
         if(lecteurActuel()!=null) System.out.println("envoi de "+mail+ " à "+lecteurActuel().getMail());
         else System.out.println("aucune location en cours");
     }
-    public void envoiMailLecteurs(Mail mail){
-        List<Lecteur>ll=lecteurs();
-        if(ll.isEmpty()){
-            System.out.println("aucun lecteur enregistré");
-        }
-        else{
-            for(Lecteur l: ll){
-                System.out.println("envoi de "+mail+ " à "+l.getMail());
-            }
-        }
-    }
-
-    public boolean enRetard(){ //par retard on entend pas encore restitué et en retard
-        if(lloc.isEmpty()) return false;
-        Location l = lloc.get(lloc.size()-1); //la location en cours est la dernière  de la liste, sauf si elle est terminée
-        if(l.getDateRestitution()==null && l.getDateLocation().plusDays(ouvrage.njlocmax()).isAfter(LocalDate.now())) return true;
-        return false;
-    }
-
-    public int joursRetard(){
-        if(!enRetard()) return 0;
-        Location l = lloc.get(lloc.size()-1);//la location en cours est la dernière de la liste
-        LocalDate dateLim = l.getDateLocation().plusDays(ouvrage.njlocmax());
-        int njretard = (int)ChronoUnit.DAYS.between(dateLim, LocalDate.now());
-        return njretard;
-    }
-
 
     public boolean enLocation(){
         if(lloc.isEmpty()) return false;
-        Location l = lloc.get(lloc.size()-1);//la location en cours est la dernière de la liste
-        if(l.getDateRestitution()==null) return true;
-        return false;
+        if(lloc.containsKey(this)) return true;
+        else return false;
     }
 
 
